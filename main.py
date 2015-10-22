817637bb59c20ab54a74cf6d669a47843ba5da43
Ujval Misra <misraujval@gmail.com>
2015-10-22 04:16:27 -0700

    pre pull

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from markov_lib import rate
 import pickle
 
 # import notes
+with open("test_notes/notes0", 'rb') as f:
+    notes0 = pickle.load(f)
 with open("test_notes/notes1", 'rb') as f:
     notes1 = pickle.load(f)
 with open("test_notes/notes2", 'rb') as f:
@@ -11,7 +13,7 @@ with open("test_notes/notes3", 'rb') as f:
     notes3 = pickle.load(f)
 
 # Calculate stochastic matrix
-matrix = stochastic_matrix(notes1, notes2, notes3, degree=2)
+matrix = stochastic_matrix(notes0, notes1, notes2, notes3, degree=2)
 
 generated_song_notes = random_walk(matrix, len(notes1), notes1, initial_note=[notes1[0], notes1[1]], degree=2)
 
--- parse_music.py
+++ parse_music.py
@@ -2,12 +2,15 @@ from markov_lib import notes_from_wav
 import pickle
 
 # Get notes from wav
-wav_list = ["fur_elise.wav", "symphony_in_am.wav", "air_on_the_g_string.wav"]
+wav_list = ["minuet_in_g_major", "fur_elise.wav", "symphony_in_am.wav", "air_on_the_g_string.wav"]
 
-notes1 = notes_from_wav(wav_list[0])
-notes2 = notes_from_wav(wav_list[1])
-notes3 = notes_from_wav(wav_list[2])
+notes0 = notes_from_wav(wav_list[0])
+notes1 = notes_from_wav(wav_list[1])
+notes2 = notes_from_wav(wav_list[2])
+notes3 = notes_from_wav(wav_list[3])
 
+with open("test_notes/notes0", 'wb') as f:
+    pickle.dump(notes0, f)
 with open("test_notes/notes1", 'wb') as f:
     pickle.dump(notes1, f)
 with open("test_notes/notes2", 'wb') as f:
